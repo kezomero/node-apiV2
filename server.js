1837a97d835bf3ffb5c1fe9a42b2f5c1f2c81211
sir derrick <dimatata01@gmail.com>
2023-08-06 23:26:38 +0300

    node api v2

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ require('dotenv').config();
 const express=require('express');
 const mongoose=require('mongoose');
 const productRoutes =require ('./routes/productRoutes');
+const errorMiddleware=require('./middleware/errorMiddleware')
+const cors = require('cors')
 
 const PORT=process.env.PORT
 const MONGO_URL=process.env.MONGO_URL
+const FRONTEND=process.env.FRONTEND
 
 const app=express();
 
+var corsOptions = {
+  origin: FRONTEND,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+//app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/api/products',productRoutes)
+
+app.get('/',(req,res)=>{
+    throw new Error (`Fake error`)
+    //res.send(`hello matata!`)
+})
+
+app.use(errorMiddleware)
 
 mongoose.
 connect(MONGO_URL)
